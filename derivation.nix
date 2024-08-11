56e375470d87cbf14dc301767f84b39e3d67aec3
Marcel <me@m4rc3l.de>
2024-08-11 21:13:41 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'website':
        'github:dd-ix/website/c6299f51ab08201a32dce19ceb1d847d71786781?narHash=sha256-MEqKIypt2B38MxdQo0TQy/A/aC4NPQUa%2BIZZce2md4Q%3D' (2024-08-10)
      → 'github:dd-ix/website/e12c414107cb78295520664a17d80972fa09764a?narHash=sha256-Ws64/OJDz4IYv/XkMsDxHZACuYIDCeggwk9IiwVZzmM%3D' (2024-08-11)
    • Updated input 'website-content-api':
        'github:dd-ix/website-content-api/8eaca6a3e3d06cbb96f4151b7b77e49f9a51d754?narHash=sha256-K3XNDBhZHDvOD78rXKG3AEOG9PRvju4O8gMLY9TJKMQ%3D' (2024-08-07)
      → 'github:dd-ix/website-content-api/348c19417f0a14bdb9ea4cc835ac2c8f21fc6fff?narHash=sha256-EgQO7LTWfHeFsMd2Nbf0wJrsnWxreLcQWQ8cG6kOqgo%3D' (2024-08-11)

--- derivation.nix
+++ derivation.nix
@@ -21,12 +21,9 @@ stdenv.mkDerivation {
     foundation --content-directory ./content/ &
     APP_DIR=${frontend} node ${frontend}/server.mjs &
     sleep 1
-    wget --recursive --page-requisites --convert-links --no-parent --continue --adjust-extension http://127.0.0.1:4000/en/|| true
-    mkdir $out
-    lndir ${frontend}/browser $out
-    (cd "127.0.0.1:4000" && find . -type f -name '*.html' -exec bash -c '(mkdir -p ${placeholder "out"}/$(dirname {}) && mv {} ${placeholder "out"}/{})' \;)
-    (cd "127.0.0.1:4000" && find . -type f -name '*.css' -exec bash -c '(mkdir -p ${placeholder "out"}/$(dirname {}) && mv {} ${placeholder "out"}/{})' \;)
-    mv "127.0.0.1:8080" $out
-    find $out -type f -name '*.js' -delete
+    mkdir data
+    wget --recursive --page-requisites --no-parent --convert-links --continue --adjust-extension http://127.0.0.1:4000/ || true
+    find '127.0.0.1:4000' -type f -iname '*.js' -delete
+    mv '127.0.0.1:4000' $out
   '';
 }
